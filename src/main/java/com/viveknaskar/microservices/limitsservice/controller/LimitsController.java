2b25f660b84b0b1715f22cb3543df4d115436821
Vivek Naskar <viveknaskar2912@gmail.com>
2021-04-09 01:32:34 +0530

    Created the initial structure

---
+++ src/main/java/com/viveknaskar/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.viveknaskar.microservices.limitsservice.controller;
+
+import com.viveknaskar.microservices.limitsservice.bean.Limits;
+import com.viveknaskar.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits fetchLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
